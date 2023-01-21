590fd49980a1fe026d84726a7331a9afb91c4d68
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-01-21 11:47:31 +0100

    Create models for users, orders & products

---
+++ src/models/product.ts
@@ -0,0 +1,77 @@
+import client from "../database";
+
+export type Product = {
+    id: number;
+    name: string;
+    price: string;
+    category: string;
+};
+
+export class ProductStore {
+    async index(): Promise<Product[]> { // a method that returns a list of all items in the database.
+        try {
+            const conn = await client.connect(); // connect to the database
+            const sql = 'SELECT * FROM products'; // write the sql query
+            const result = await conn.query(sql); // run the sql query on the database
+            conn.release(); // close database connection
+            return result.rows; // return the rows contained in the database query result   
+        } catch (error) {
+            throw new Error(`Error fetching data: ${error}`);
+        }
+
+    }
+
+    async show(id: string): Promise<Product> {
+        try {
+            const sql = 'SELECT * FROM products WHERE id=($1)'
+            // @ts-ignore
+            const conn = await client.connect()
+
+            const result = await conn.query(sql, [id])
+
+            conn.release()
+
+            return result.rows[0]
+        } catch (err) {
+            throw new Error(`Could not find item with id ${id}. Error: ${err}`)
+        }
+    }
+
+    async create(p: Product): Promise<Product> {
+        try {
+            const sql =
+                'INSERT INTO products (name, price, category) VALUES($1, $2, $3) RETURNING *'
+            // @ts-ignore
+            const conn = await client.connect()
+
+            const result = await conn
+                .query(sql, [p.name, p.price, p.category])
+
+            const weapon = result.rows[0]
+
+            conn.release()
+
+            return weapon
+        } catch (err) {
+            throw new Error(`Could not add new book ${p.name}. Error: ${err}`)
+        }
+    }
+
+    async delete(id: string): Promise<Product> {
+        try {
+            const sql = 'DELETE FROM products WHERE id=($1)'
+            // @ts-ignore
+            const conn = await client.connect()
+
+            const result = await conn.query(sql, [id])
+
+            const product = result.rows[0]
+
+            conn.release()
+
+            return product;
+        } catch (err) {
+            throw new Error(`Could not delete book ${id}. Error: ${err}`)
+        }
+    }
+}
---
+++ src/models/user.ts
@@ -0,0 +1,89 @@
+import bcrypt from 'bcrypt';
+import client from '../database';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+
+const pepper = process.env.BCRYPT_PASSWORD;
+const saltRounds = process.env.SALT_ROUNDS;
+
+export type User = {
+    id: number,
+    firstName: string,
+    lastName: string,
+    username: string,
+    password: string
+}
+
+export class Users {
+    async index(): Promise<User[]> {
+        try {
+            // @ts-ignore
+            const conn = await client.connect();
+            const sql = 'SELECT * FROM users';
+            const users = await conn.query(sql);
+            conn.release()
+            return users.rows;
+        } catch (error) {
+            throw new Error(`${error}`);
+        }
+    }
+
+    async show(username: string): Promise<User> {
+        try {
+            // @ts-ignore
+            const conn = await client.connect();
+            const sql = 'SELECT * FROM users WHERE username=($1)';
+            const user = await conn.query(sql, [username]);
+            conn.release();
+            return user.rows[0];
+        } catch (error) {
+            throw new Error(`${error}`);
+        }
+    }
+
+    async create(u: User): Promise<User> {
+        try {
+            // @ts-ignore
+            const conn = await client.connect();
+            const sql = 'INSERT INTO users (firstName, lastName, username, password_hash) VALUES ($1, $2, $3, $4) RETURNING *';
+            const password_hash = bcrypt.hashSync(
+                u.password + pepper,
+                parseInt(saltRounds!)
+            );
+
+            const result = await conn.query(sql, [u.firstName, u.lastName, u.username, password_hash]);
+            const user = result.rows[0];
+
+            conn.release();
+            return user;
+        } catch (error) {
+            throw new Error(`Unable to create user (${u.username}): ${error}`);
+        }
+    }
+
+    async authenticate(username: string, password: string): Promise<User | null> {
+        try {
+            // @ts-ignore
+            const conn = await client.connect();
+            const sql = 'SELECT password FROM users WHERE username=($1)';
+            const result = await conn.query(sql, [username]);
+
+            console.log(password + pepper);
+
+            if (result.rows.length) {
+                const user = result.rows[0];
+                console.log(user);
+
+                if (bcrypt.compareSync(password + pepper, user.password)) {
+                    return user;
+                }
+            }
+
+            return null;
+        } catch (error) {
+            throw new Error(`Authentication failed for user (${username}): ${error}`);
+        }
+    }
+}
