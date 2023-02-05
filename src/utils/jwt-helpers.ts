4128fa7c814486c622df6b7888a4a78b5ac4d624
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-02-05 22:00:23 +0100

    Update jwt signature & verification

--- src/utils/jwt-helpers.ts
+++ src/utils/jwt-helpers.ts
@@ -1,9 +1,9 @@
 import jwt from 'jsonwebtoken';
 import { User } from '../models/user'
 
-function jwtTokens({ id, firstName, lastName, username }: User) {
-    const user = { id, firstName, lastName, username };
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET!, { expiresIn: '5m' });
+function jwtTokens(user: User) {
+    // const user = { id, firstName, lastName, username };
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET!, { expiresIn: '2h' });
     return { accessToken };
 }
 
