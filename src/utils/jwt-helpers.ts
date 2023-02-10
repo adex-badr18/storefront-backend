459e1174634a283c1d2228a7f6e25eb6cdba8090
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-02-10 04:55:27 +0100

    Update jwt expiration time

--- src/utils/jwt-helpers.ts
+++ src/utils/jwt-helpers.ts
@@ -3,7 +3,7 @@ import { User } from '../models/user'
 
 function jwtTokens(user: User) {
     // const user = { id, firstName, lastName, username };
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET!, { expiresIn: '2h' });
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET!, { expiresIn: '10h' });
     return { accessToken };
 }
 
